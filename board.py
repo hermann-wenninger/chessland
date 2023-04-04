39517e1903dbce245a8d4b25ef8cdaf18acb9b18
hermann-wenninger <hermann23@vivaldi.net>
2023-04-04 19:21:48 +0200

    representants

--- board.py
+++ board.py
@@ -9,12 +9,16 @@ board[::2,1::2] = 1
 Filo = ["a", "b", "c", "d", "e", "f", "g", "h"]
 Numbr = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
-filonum = [f+n for f in Filo for n in Numbr]
+filonum = [f+n for f in Numbr for n in Filo]
 filonum = np.array(filonum, dtype = np.chararray).reshape(8,8)[::-1]
+
+
+
+
 
 
 print(one_to_64)
 print(ones)
 print(zeros)
-print(board[::-1])
+print(board)
 print(filonum)
