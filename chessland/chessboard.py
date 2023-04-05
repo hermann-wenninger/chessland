6da2aef587068066bce133b428d09d2e15378ba0
hermann <hermann23@vivaldi.net>
2023-04-05 17:27:50 +0200

    ordering

--- chessland/chessboard.py
+++ chessland/chessboard.py
@@ -1,16 +1,17 @@
 #CHESSBOARD
-#CHESSBOARD
+
 import numpy as np
 import cv2
 from PIL import Image
+from numpy import asarray
 
 
-im = cv2.imread('k.png')
-img = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)  
-print(img)
-print(np.shape(img))
-front = (img[:,:,0] / 255).astype(int)
-print(np.shape(front))
+#im = cv2.imread('k.png')
+#img = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)  
+#print(img)
+#print(np.shape(img))
+#front = (img[:,:,0] / 255).astype(int)
+#print(np.shape(front))
 
 a = input('spieler 1:')
 b = input('spieler 2:')
@@ -22,7 +23,7 @@ def createboard(a, b):
     spielfeldgr = 640
     feld = int(spielfeldgr/8)
     npo = np.zeros((spielfeldgr,spielfeldgr)) 
-    print(npo)
+    #print(npo)
     cb = 0
     cw = 1
     r1 = 0
@@ -34,7 +35,7 @@ def createboard(a, b):
             if i % 2 == 0:
                 if j % 2 == 0:
                     npo[r:c, r1:c1]=[cw]
-                    print(npo)
+                    #print(npo)
                 else:
                     npo[r:c, r1:c1]=[cb]
             else:
@@ -47,17 +48,20 @@ def createboard(a, b):
         r1 = r1 + feld
         c1 = c1 + feld
    
-        img1 = Image.fromarray(np.uint8(npo * 255) , 'L')
-        img2 = Image.open('k.png')
-        img1.paste(img2)
-        img1.save('paste.jpg', quality=95)
+    img1 = Image.fromarray(np.uint8(npo * 255) , 'L')
+    img2 = Image.open('../images/n.png')
+    img1.paste(img2,(160,160))
+    img1.show(titel)
+    data = asarray(img1)
+    cv2.imshow(titel, data)
+    #img1.save('paste.jpg', quality=95)
         
 
-    cv2.imshow(titel, npo)
+    #cv2.imshow(titel, npo)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-    print(npo)
+    #print(npo)
 
 
 createboard(a, b)
