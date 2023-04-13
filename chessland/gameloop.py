0dceaea638d28bc7fb9230e4917431b2a9a15b23
hermann <hermann23@vivaldi.net>
2023-04-13 16:14:06 +0200

    iterate over colors sort by rank

--- chessland/gameloop.py
+++ chessland/gameloop.py
@@ -18,29 +18,90 @@ zug = 1
 #BLACKPIECES = ['br','bn','bb','bq','bk','bp']
 ALLPIECES = WHITEPIECES + BLACKPIECES
 
-def iterate_over_board(start, COLOR_OF_PICES):
+def iterate_over_black(start, COLOR_OF_PICES):
+    '''iterate over all pices from one black pices and give back 
+    the number of the 120er board 
+    the identity of sqares
+    the rank of the pice
+    '''
     print('iterate over board')
+    OUTCOME_B = []
+    justnum_b = []
     for i , x in enumerate(start):
-        for j, y in enumerate(x):
-            print(i,j,y)
+        for j, pice in enumerate(x):
+            if pice in COLOR_OF_PICES:
+                squarenum = TABLE[str(i)+str(j)]
+                #OUTCOME is a list with data from black or white[pice boardposition 120ernummer]
+                OUTCOME_B.append([pice,SQUARESSWITCH[squarenum],squarenum])
+                justnum_b.append(squarenum)
+    print(OUTCOME_B)
+    print(justnum_b)
+    print(len(OUTCOME_B))
+    return OUTCOME_B, justnum_b
+
+
+def iterate_over_white(start, COLOR_OF_PICES):
+    '''iterate over all pices from one whitepices and give back 
+    the number of the 120er board 
+    the identity of sqares
+    the rank of the pice
+    '''
+    print('iterate over white board')
+    OUTCOME_W = []
+    justnum_w = []
+    for i , x in enumerate(start):
+        for j, pice in enumerate(x):
+            if pice in COLOR_OF_PICES:
+                squarenum = TABLE[str(i)+str(j)]
+                #OUTCOME is a list with data from black or white[pice boardposition 120ernummer]
+                OUTCOME_W.append([pice,SQUARESSWITCH[squarenum],squarenum])
+                justnum_w.append(squarenum)
+    print(OUTCOME_W)
+    print(justnum_w)
+    print(len(OUTCOME_W))
+    return OUTCOME_W, justnum_w
+
+
+
+def take_spezial_pices(pool,color_rank):
+    '''extrakt the OUTCOME list by color and rank'''
+    ranklist = [i for i in pool if i[0]== color_rank]
+    print(ranklist)
+    return ranklist
+
 
 
 def zug_black():
     print('zug schwarz')
-    iterate_over_board(start, BLACKPIECES)
+    x, y = iterate_over_black(start, BLACKPIECES)
+    take_spezial_pices(x, 'bp')
    
 
 def zug_white():
     print('zug weiß')
-    iterate_over_board(start, WHITEPIECES)
+    x, y = iterate_over_white(start, WHITEPIECES)
+    take_spezial_pices(x, 'wp')
+   
 
 while w_king_alive and b_king_alive:
+    iterate_over_black(start, BLACKPIECES)
     if zug % 2 == 0:
        
         zug_black()
     else:
         print('')
-        #zug_white()
-    if zug > 10:
+        zug_white()
+    if zug > 2:
         break
     zug +=1
+
+
+
+def whitepawn_start(getnum):
+    '''first step of a pawn'''
+    a = getnum + 9
+    b = getnum +11
+    x = getnum + 10
+    y = getnum + 20
+
+
